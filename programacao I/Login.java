1e97044e3599c423a3e18f48ccf5d8016aaabd2b
Weslen <weslwnrodrigues@gmail.com>
2026-03-29 18:31:14 -0300

    .

---
+++ programacao I/Login.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Login {
+    public static void main (String [] args) {
+
+        String usuario = "", confirmandoUsuario = "";
+        String senha = "", confirmandoSenha = "";
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Insira seu usuário");
+        usuario = scan.nextLine();
+
+        System.out.println("Insira sua senha");
+        senha = scan.nextLine();
+
+        do {
+        System.out.println("Confirme seu usuário");
+        confirmandoUsuario = scan.nextLine();
+
+        System.out.println("Confirme sua senha");
+        confirmandoSenha = scan.nextLine();
+
+        if (!confirmandoUsuario.equals(usuario)) {
+            System.out.println("Usuário incorreto");
+        }
+
+        if (!confirmandoSenha.equals(senha)) {
+            System.out.println("Senha incorreta");
+        }
+
+        } while (!confirmandoUsuario.equals(usuario) || !confirmandoSenha.equals(senha));
+
+        System.out.println("Logado com sucesso");
+
+        scan.close();
+    }
+}
